a5bd04bce3b3d651b427032e8e5106ce90b9fbf0
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-08-01 12:18:47 +0200

    Aggiornato

--- selfwork2.php
+++ selfwork2.php
@@ -1,66 +1,62 @@
 <?php
 
-// $words1 = [
-//     'vostro',
-//     67,
-//     'essere',
-//     'colle',
-//     'mi',
-//     'sempre',
-//   [
-//     'oscura',
-//     'era',
-//     89,
-//     [
-//     'mezzo',
-//      'E'
-//     ],
-//   'ritrovai',
-//   'per'
-//   ],
-//   'diritta'
-//   ];
-  
-//   $words2 = [
-//     'elemento1' => 25.89,
-//     'elemento2' => 'possa',
-//     'elemento3' => [
-//       'Virgilio',
-//       'Favore',
-//       'favore',
-//       ['fortuna']
-//     ],
-//    'fine' => '!'
-//   ];
-  
+$words1 = [
+  'vostro',
+  67,
+  'essere',
+  'colle',
+  'mi',
+  'sempre',
+  [
+    'oscura',
+    'era',
+    89,
+    [
+      'mezzo',
+      'E'
+    ],
+    'ritrovai',
+    'per'
+  ],
+  'diritta'
+];
 
-
-$result = [
-
-$text1 = 'Nel',
-$text2 = 'Mezzo',
-$text3 = 'del ',
-$text4 = 'cammin',
-$text5 = 'di',
-$text6 = 'nostra',
-$text7 = 'vita',
-$text8 = 'mi',
-$text9 = 'ritrovai',
-$text10 = 'in',
-$text11 = 'una',
-$text12 = 'selva',
-$text13 = 'oscura',
-$text14 = 'che',
-$text15 = 'la',
-$text16 = 'dritta',
-$text17 = 'via',
-$text18 = 'era',
-$text19 = 'smarrita',
-
+$words2 = [
+  'elemento1' => 25.89,
+  'elemento2' => 'possa',
+  'elemento3' => [
+    'Virgilio',
+    'Favore',
+    'favore',
+    ['fortuna']
+  ],
+  'fine' => '!'
 ];
 
 
-var_dump($result);
+// --------------- \\
 
+$results = "Nel " 
+  . $words1[6][3][0] . " "
+  . "di "
+  . "cammin "
+  . "di "
+  . "nostra "
+  . "vita "
+  . $words1[4] . " "
+  . $words1[6][4] . " "
+  . $words1[6][5] . " "
+  . "una "
+  . "selva "
+  . $words1[6][0] . ", "
+  . "che' "
+  . "la "
+  . $words1[7] . " "
+  . "via "
+  . $words1[6][1] . " "
+  . "smarrita.";
+
+
+echo $results;
 
 ?>

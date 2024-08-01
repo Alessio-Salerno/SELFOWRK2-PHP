a24c93a2ef74f96f30eee1cd70cf0ab9a68086b6
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-08-01 11:26:29 +0200

    Add files via upload

---
+++ selfwork2.php
@@ -0,0 +1,66 @@
+<?php
+
+// $words1 = [
+//     'vostro',
+//     67,
+//     'essere',
+//     'colle',
+//     'mi',
+//     'sempre',
+//   [
+//     'oscura',
+//     'era',
+//     89,
+//     [
+//     'mezzo',
+//      'E'
+//     ],
+//   'ritrovai',
+//   'per'
+//   ],
+//   'diritta'
+//   ];
+  
+//   $words2 = [
+//     'elemento1' => 25.89,
+//     'elemento2' => 'possa',
+//     'elemento3' => [
+//       'Virgilio',
+//       'Favore',
+//       'favore',
+//       ['fortuna']
+//     ],
+//    'fine' => '!'
+//   ];
+  
+
+
+$result = [
+
+$text1 = 'Nel',
+$text2 = 'Mezzo',
+$text3 = 'del ',
+$text4 = 'cammin',
+$text5 = 'di',
+$text6 = 'nostra',
+$text7 = 'vita',
+$text8 = 'mi',
+$text9 = 'ritrovai',
+$text10 = 'in',
+$text11 = 'una',
+$text12 = 'selva',
+$text13 = 'oscura',
+$text14 = 'che',
+$text15 = 'la',
+$text16 = 'dritta',
+$text17 = 'via',
+$text18 = 'era',
+$text19 = 'smarrita',
+
+];
+
+
+var_dump($result);
+
+
+?>
